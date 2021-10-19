4d689b5c074ca07682e8ab25a760ac2ce487b65d
AbdullahShion <69447798+AbdullahShion@users.noreply.github.com>
2021-10-19 20:05:56 +0600

    second

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+// This is index hs file
 ReactDOM.render(
   <React.StrictMode>
     <App />
